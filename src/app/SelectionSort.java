4fbaf637e17719256fe9892d976987fa28adae5b
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-15 11:42:38 +0700

    add project

---
+++ src/app/SelectionSort.java
@@ -0,0 +1,31 @@
+package app;
+
+public class SelectionSort {
+	public static void selectionSort(double[] list) {
+		for (int i = 0; i < list.length - 1; i++) {
+			/* Find the minimum in the list[i..list.length-1] */
+			double currentMin = list[i];
+			int currentMinIndex = i;
+
+			for (int j = i + 1; j < list.length; j++) {
+				if (currentMin > list[j]) {
+					currentMin = list[j];
+					currentMinIndex = j;
+				}
+			}
+
+			/* Swap list[i] with list[currentMinIndex] if necessary */
+			if (currentMinIndex != i) {
+				list[currentMinIndex] = list[i];
+				list[i] = currentMin;
+			}
+		}
+		// Complexity O(n2)
+	}
+	public static void main(String[] args) {
+		double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+		selectionSort(list);
+		for (int i = 0; i < list.length; i++)
+			System.out.print(list[i] + " ");
+	}
+}
